7a988f2ad0ede83edd82d1db19a2be56c1e232b4
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-08-01 12:40:24 -0400

    Views for post detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 import json
@@ -10,3 +10,8 @@ from django.core.serializers.json import DjangoJSONEncoder
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
